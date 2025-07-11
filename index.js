6cf122a7bca96d580d65e6087cb54cf8f3f0d9e9
annaanan11 <seriko0614@gmail.com>
2025-07-11 23:19:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ const passwordAccessRules = {
   const embed = new EmbedBuilder()
     .setColor(0x00cc66)
     .setTitle("嫣懨角色資料")
-    .setDescription(`🔐 密碼：\`8641\``\n🔗 [角色網頁](https://abr.ge/ew63bq)`);
+    .setDescription(`🔐 密碼：\`8641\`\n🔗 [角色網頁](https://abr.ge/ew63bq)`);
 
   try {
     await message.author.send({ embeds: [embed] });
