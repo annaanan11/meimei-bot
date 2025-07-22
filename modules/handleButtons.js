8dbefbbdc760a3e222d5311565105eaa20f6fb0c
annaanan11 <seriko0614@gmail.com>
2025-07-23 02:28:40 +0800

    Update handleButtons.js

--- modules/handleButtons.js
+++ modules/handleButtons.js
@@ -108,7 +108,7 @@ async function sendRoleEmbedButtons(message, roleGroups) {
       }
     }
 
-    try {
+        try {
       await message.channel.send({
         embeds: [embed],
         components: rows
@@ -116,6 +116,7 @@ async function sendRoleEmbedButtons(message, roleGroups) {
     } catch (err) {
       console.error(`❌ 發送角色群組「${group.title}」失敗：`, err);
     }
+  }
 }
 
 module.exports = {
